54ae6e23d767c4bbe2fe107e6cdc4248ea97198b
Jitse-Jan <code@jitsejan.com>
2019-07-05 10:59:04 +0100

    Started using pytest

--- tests/test_data_crawl.py
+++ tests/test_data_crawl.py
@@ -1,5 +1,6 @@
 import lxml.html
 import os
+import pytest
 import sys
 import unittest
 import unittest.mock as mock
@@ -21,30 +22,24 @@ class DataCrawlTestClass(unittest.TestCase):
         expected = "https://mario.nintendo.com"
         actual = self.object_to_test.BASE_URL
 
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
     @mock.patch("data_crawl.basecrawler.BaseCrawler._get_copyright", autospec=True)
     def test_if_get_copyright_is_called_with_correct_types(self, mocked_method):
         self.object_to_test._get_copyright(self.elem)
         (func, call_arg) = mocked_method.call_args[0]
 
-        self.assertEqual(type(call_arg), lxml.html.HtmlElement)
+        assert type(call_arg) == lxml.html.HtmlElement
 
     def test_base_class_cannot_be_inited_with_params(self):
-        expected = "_get_image_from_elem() missing 2 required positional arguments: 'elem' and 'selector'"
-        with self.assertRaises(TypeError) as context:
+        expected = r"_get_image_from_elem\(\) missing 2 required positional arguments: \'elem\' and \'selector\'"
+        with pytest.raises(TypeError, match=expected):
             self.object_to_test._get_image_from_elem()
-        actual = str(context.exception)
-
-        self.assertEqual(expected, actual)
 
     def test_base_class_no_get_data_implemented(self):
-        expected = "get_data() is not implemented"
-        with self.assertRaises(NotImplementedError) as context:
+        expected = r"get_data\(\) is not implemented"
+        with pytest.raises(NotImplementedError, match=expected):
             self.object_to_test.get_data()
-        actual = str(context.exception)
-
-        self.assertEqual(expected, actual)
 
     @mock.patch("data_crawl.charactercrawler.CharacterCrawler.get_data", autospec=True)
     def test_derived_class_get_data_is_called_twice(self, mocked_method):
@@ -53,7 +48,7 @@ class DataCrawlTestClass(unittest.TestCase):
         subject.get_data()
         subject.get_data()
 
-        self.assertEqual(mocked_method.call_count, 2)
+        assert mocked_method.call_count == 2
 
 
 if __name__ == "__main__":
