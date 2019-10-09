6e2abe6f3ba81b99f0e30684625543dfac80f256
Jitse-Jan <code@jitsejan.com>
2019-10-10 00:00:53 +0100

    Small update on tests

--- tests/test_charactercrawler.py
+++ tests/test_charactercrawler.py
@@ -1,12 +1,12 @@
 import data_crawl
 
 class TestCharacterCrawler:
-    """ Defines the tests for the base class for the CharacterCralwer """
+    """ Defines the tests for the base class for the CharacterCrawler """
 
     def test_derived_class_get_data_is_called_twice(self, mocker, charactercrawler):
         """ Test if the derived class get data function is called twice """
         mocked_get_data = mocker.patch("data_crawl.charactercrawler.CharacterCrawler.get_data", autospec=True)
-        charactercrawler.get_data()
-        charactercrawler.get_data()
+        result_one = charactercrawler.get_data()
+        result_two = charactercrawler.get_data()
 
         assert mocked_get_data.call_count == 2
