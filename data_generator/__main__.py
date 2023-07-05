d4f96445ba0e5df8502849861eb49dbf6162cabc
Jitse-Jan <code@jitsejan.com>
2023-07-05 03:19:57 +0100

    Added requirements and Python version

--- data_generator/__main__.py
+++ data_generator/__main__.py
@@ -5,9 +5,10 @@ import argparse
 import datetime
 from dateutil.relativedelta import relativedelta
 
+CHARACTERS = ["Mario", "Luigi", "Peach", "Toad"]
 DATE_END = datetime.datetime.now()
 DATE_START = DATE_END + relativedelta(months=-1)
-OUTPUT_CHOICES = ["jl", "list"]
+OUTPUT_CHOICES = ["jl", "csv", "list"]    
 
 
 def _get_parser():
@@ -21,7 +22,13 @@ def _get_parser():
         "-n", "--num-events", type=int, default=10
     )
     parser.add_argument(
-        "-o", "--output", nargs="?", choices=OUTPUT_CHOICES, required=True
+        "-ot", "--output_type", nargs="?", choices=OUTPUT_CHOICES, default="list"
+    )
+    parser.add_argument(
+        "-of", "--output_file", default="data.output"
+    )
+    parser.add_argument(
+        "-ch", "--characters", nargs='+', default=CHARACTERS
     )
     return parser
 
@@ -32,16 +39,18 @@ def main():
     parser = _get_parser()
     args = parser.parse_args()
     params = {
+        "characters": args.characters,
         "num_events": args.num_events,
-        "output_type": args.output,
-        "output_file": "characterdata.jl",
+        "output_type": args.output_type,
+        "output_file": args.output_file,
         "start_date": DATE_START,
         "end_date": DATE_END,
     }
+    print(params)
     # Create the event generator
     generator = EventGenerator(**params)
     # Create and store the events
-    generator.store_events()
+    result = generator.store_events()
 
 
 if __name__ == "__main__":
